87a28dadb17d49a20969e255ef2e3584f69d5a35
Mikaela Bell <19698567+mikaelabelle@users.noreply.github.com>
2024-04-22 11:23:00 +1000

    more JS

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const myLibrary = []
 const newBookForm = document.querySelector("#newBook")
 const titleInput = document.querySelector("#title")
-const authorInput = document.querySelector("author")
-const pagesInput = document.querySelector("pages")
+const authorInput = document.querySelector("#author")
+const pagesInput = document.querySelector("#pages")
+const libraryDiv = document.querySelector("#library")
 
 function Book(title, author, pages, readStatus) {
     this.title = title;
@@ -17,14 +18,21 @@ function Book(title, author, pages, readStatus) {
 function addBookToLibrary(title, author, pages, readStatus) {
     const newBook = new Book(title, author, pages, readStatus)
     myLibrary.push(newBook)
+    addBookCard(newBook)
+}
+
+function addBookCard(book) {
+    const bookCard = document.createElement("div")
+    const title = document.createTextNode(`${book.title} by ${book.author}`)
+    bookCard.appendChild(title)
+    bookCard.classList.add("book")
+    libraryDiv.appendChild(bookCard)
 }
 
 addBookToLibrary("Le Test", "Person Personus", 3243, "not read yet")
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
 
 newBookForm.addEventListener("submit", e => {
-    console.log(titleInput.value)
+    e.preventDefault()
+    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value)
 })
-
-myLibrary.forEach(book => console.log(book.info()))
-
